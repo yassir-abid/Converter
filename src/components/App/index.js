b10cbfc9e1daa2ce2a25578a8f5140c91a36ad0e
yassir-abid <abid.yassir@gmail.com>
2023-01-23 15:27:37 +0100

    turn app component into class

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,30 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
 import Header from 'src/components/Header';
 import Currencies from 'src/components/Currencies';
 import Result from 'src/components/Result';
 import currenciesList from 'src/data/currencies';
 import './styles.scss';
 
-function App() {
-  return (
-    <div className="app">
-      <Header baseAmount={1} />
-      <Currencies currencies={currenciesList} />
-      <Result value={1.09} selectedCurrency="United States Dollar" />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header baseAmount={1} />
+        <Currencies currencies={currenciesList} />
+        <Result value={1.09} selectedCurrency="United States Dollar" />
+      </div>
+    );
+  }
 }
 
 export default App;
